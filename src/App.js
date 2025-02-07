f047ad24febf74c48eea83adda27618da4cb2a11
BabithaVakiti <saikrishna26031998@gmail.com>
2025-02-07 17:54:58 -0600

    adding login page features

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Loginpage from './Components/Pages/LoginForm/Loginpage';
+//import Sidebar from './Components/Layouts/Sidebar';
+
+function App() {
+  return (
+    <div>
+      <Loginpage />
+    </div>
+  );
+}
+
+export default App;
