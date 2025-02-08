f1464d693c46eeba726540b3bf495218c0637c5b
BabithaVakiti <113443893+BabithaVakiti@users.noreply.github.com>
2025-02-07 20:54:45 -0600

    Adding Router functionality  for Login and register page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Loginpage from './Components/Pages/LoginForm/Loginpage';
+import RegisterForm from './Components/Pages/RegisterForm/RegisterPage';
 //import Sidebar from './Components/Layouts/Sidebar';
 
 function App() {
   return (
-    <div>
-      <Loginpage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" component={<Loginpage/>} />
+        <Route path="/Registerpage" component={<RegisterForm/>} />
+      </Routes>
+    </Router>
   );
 }
 
