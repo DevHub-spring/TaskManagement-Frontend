1d6f3828f71f84323dccf9c4bda1831927ffaeb1
BabithaVakiti <113443893+BabithaVakiti@users.noreply.github.com>
2025-02-09 21:07:15 -0600

    Adding forgot password Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
+ import React from 'react';
 import Loginpage from './Components/Pages/LoginForm/Loginpage';
 import RegisterForm from './Components/Pages/RegisterForm/RegisterPage';
 import ForgotPage from './Components/Pages/ForgotForm/ForgotPage';
 import {BrowserRouter as Router, Route, Routes,Navigate} from 'react-router-dom';
+import SideBar from './Components/Pages/SideBar/SideBarPage';
 import './App.css';
-//import Sidebar from './Components/Layouts/Sidebar';
 
-function App() {
+const App = () => {
   return (
-
-    <Router>
-      <Routes>
-      <Route path="/" element={<Navigate to="/loginpage" />} /> {/* Redirect to /loginpage */}
-        <Route path="/loginpage" element={<Loginpage/>} />
-        <Route path="/registerpage" element={<RegisterForm/>} />
-        <Route path="/forgotpage" element={<ForgotPage/>} />
-      </Routes>
-    </Router>
+      <div>
+        <SideBar />
+        <main>
+          <h2>Content</h2>
+        </main>
+      </div>
+    // <Router>
+    //   <Routes>
+    //   <Route path="/" element={<Navigate to="/loginpage" />} /> {/* Redirect to /loginpage */}
+    //     <Route path="/loginpage" element={<Loginpage/>} />
+    //     <Route path="/registerpage" element={<RegisterForm/>} />
+    //     <Route path="/forgotpage" element={<ForgotPage/>} />
+    //   </Routes>
+    // </Router>
   );
 }
 
