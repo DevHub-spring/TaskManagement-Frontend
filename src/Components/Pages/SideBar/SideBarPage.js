a3870f00fb08143879fdf903b78ba8fe54dff679
BabithaVakiti <113443893+BabithaVakiti@users.noreply.github.com>
2025-02-09 21:34:02 -0600

    Adding side bar pages

--- src/Components/Pages/SideBar/SideBarPage.js
+++ src/Components/Pages/SideBar/SideBarPage.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import {useState} from 'react';
 import './SideBarPage.css';
+import "remixicon/fonts/remixicon.css";
 
 
-export const SideBarPage = () => {
+const SideBarPage = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  // eslint-disable-next-line no-sparse-arrays
+  const menuItems=[
+    { icon: "ri-dashboard-3-line", name: "Home" },
+    { icon: "ri-rocket-line", name: "AllTasks" },
+    { icon: "ri-account-box-line", name: "Account" },
+  ];
   return (
-    <div>SideBarPage</div>
+    <nav className="nav">
+      <ul className="nav__list">
+      {menuItems.map((item, index) => (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a
+            href="#"
+            key={index}
+            className={`nav__link ${activeIndex === index ? "active-link" : ""}`}
+            onClick={() => setActiveIndex(index)}
+          >
+            <i className={item.icon}></i>
+            <span className="nav__name">{item.name}</span>
+          </a>
+        ))}
+      </ul>
+      {/* Shapes */}
+      <div className="nav__circle-1"></div>
+      <div className="nav__circle-2"></div>
+      <div className="nav__square-1"></div>
+      <div className="nav__square-2"></div>
+    </nav>
   )
 }
 export default SideBarPage;
